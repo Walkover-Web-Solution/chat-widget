3cda4a7647fa0f9c8a4096b26ec3c3f5bbde5dcd
Rudra007 <rudrakshkachhawa@gmail.com>
2025-05-30 12:22:15 +0530

    bugFix -> dont send key with falsy values in user data

--- components/Chatbot-Wrapper/ChatbotWrapper.tsx
+++ components/Chatbot-Wrapper/ChatbotWrapper.tsx
@@ -116,7 +116,7 @@ function ChatbotWrapper({ chatbotId }: ChatbotWrapperProps) {
         setLocalStorage('client', JSON.stringify({ mail: clientMail, number: clientNumber, name: clientName, country_code: clientCountryCode || "+91" }));
       }
 
-      setLocalStorage('userData', JSON.stringify({ unique_id, mail, number, user_jwt_token: hasUserIdentity ? user_jwt_token : undefined, name }));
+      setLocalStorage('userData', JSON.stringify({ unique_id, mail, number, user_jwt_token,name }));
 
       // 4. Anonymous cleanup when no identity
       if (!hasUserIdentity && getLocalStorage('k_clientId')) {
