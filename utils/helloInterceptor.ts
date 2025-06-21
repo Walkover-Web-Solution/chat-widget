5d5767b129cccf2bf3a4de5a7df9359980ddef02
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2025-06-22 01:31:04 +0530

    client creation on runtime after client deletion (#226)

--- utils/helloInterceptor.ts
+++ utils/helloInterceptor.ts
@@ -1,8 +1,4 @@
 import defaultAxios from "axios";
-import { getLocalStorage, setLocalStorage } from "./utilities";
-import { store } from "../store";
-import { setHelloKeysData } from "../store/hello/helloSlice";
-import { setDataInAppInfoReducer } from "@/store/appInfo/appInfoSlice";
 
 const instance = defaultAxios.create();
 const axios = instance;
@@ -12,35 +8,35 @@ axios.interceptors.response.use(
   (response) => {
     return response;
   },
-  async function (error) {
-    if (error?.response?.data?.message?.is_exists === 0) {
-      let is_anon = ''
-      if (getLocalStorage('k_clientId')) {
-        is_anon = 'false'
-      } else {
-        is_anon = 'true'
-      }
+  // async function (error) {
+  //   if (error?.response?.data?.message?.is_exists === 0) {
+  //     let is_anon = ''
+  //     if (getLocalStorage('k_clientId')) {
+  //       is_anon = 'false'
+  //     } else {
+  //       is_anon = 'true'
+  //     }
 
-      store.dispatch(setDataInAppInfoReducer({
-        subThreadId: ''
-      }))
+  //     store.dispatch(setDataInAppInfoReducer({
+  //       subThreadId: ''
+  //     }))
 
-      store.dispatch(setHelloKeysData({
-        currentChannelId: '',
-        currentChatId: '',
-        currentTeamId: ''
-      }))
+  //     store.dispatch(setHelloKeysData({
+  //       currentChannelId: '',
+  //       currentChatId: '',
+  //       currentTeamId: ''
+  //     }))
 
-      setLocalStorage('is_anon', is_anon)
-      setLocalStorage('a_clientId', '')
-      setLocalStorage('k_clientId', '')
-      setLocalStorage('default_client_created', 'false')
-      const currentWidgetId = getLocalStorage('WidgetId') || ''
-      setLocalStorage('WidgetId', currentWidgetId)
+  //     setLocalStorage('is_anon', is_anon)
+  //     setLocalStorage('a_clientId', '')
+  //     setLocalStorage('k_clientId', '')
+  //     setLocalStorage('default_client_created', 'false')
+  //     const currentWidgetId = getLocalStorage('WidgetId') || ''
+  //     setLocalStorage('WidgetId', currentWidgetId)
 
-    }
-    return Promise.reject(error);
-  }
+  //   }
+  //   return Promise.reject(error);
+  // }
 );
 
 export default axios;
