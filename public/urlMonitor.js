c19baca869185d0c16e638c51680058f718e2ebb
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2025-07-10 15:27:00 +0530

    Revert "urlMonitor redirect issue"

--- public/urlMonitor.js
+++ public/urlMonitor.js
@@ -1,7 +1,4 @@
 (function () {
-    if (window.__urlMonitorInitialized__) return;
-    window.__urlMonitorInitialized__ = true;
-
     let trackedUrls = [];
     let ignoreNextOpen = false;  // Flag to skip interception once
 
