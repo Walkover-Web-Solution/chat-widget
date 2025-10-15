9458d9ad9685db305ed696fad7abb3357c39acc6
Ashish Yadav <ashish@walkover.in>
2025-10-15 15:51:39 +0530

    fix overriding  height issue and add esc to close

--- public/chat-widget-local.js
+++ public/chat-widget-local.js
@@ -336,7 +336,9 @@
             const tempContainer = document.createElement('body');
             
             // Style it to be invisible but measurable
-            tempContainer.style.position = 'absolute';            
+            tempContainer.style.position = 'absolute !important';
+            tempContainer.style.height = 'initial !important';
+            tempContainer.style.width = 'initial !important';
             
             // Set the HTML content
             tempContainer.innerHTML = htmlContent;
@@ -392,6 +394,13 @@
                 // Add click event to close button
                 closeButton.addEventListener('click', () => {
                     this.removeNotification(overlay);
+                });
+
+                // Close popup when pressing ESC key
+                document.addEventListener('keydown', (e) => {
+                    if (e.key === 'Escape') {
+                        this.removeNotification(overlay);
+                    }
                 });
 
                 // Set position classes based on horizontal and vertical position values
@@ -563,6 +572,7 @@
                     iframeDoc.close();
                 }, 0);
             }
+
         }        
 
         removeNotification(overlayElement) {
